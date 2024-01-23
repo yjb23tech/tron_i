2340544baa58de45d93ad24d1f350b4b2ef08114
yjb23tech <yjb23tech@gmail.com>
2024-01-23 14:55:59 +0000

    Quavo

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import {createRoot} from "react-dom/client"
+
+const App = () => {
+    return (
+        <div>
+            <h1>Sanity Check</h1>
+        </div>
+    )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
